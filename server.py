f470f8845dbfda7843e0e04dfb8cb67b1b2fbbb8
Steve Milner <stevem@gnulinux.net>
2013-10-30 16:09:19 -0400

    Now using wsgiref instead of tornado for simplicity.

--- server.py
+++ server.py
@@ -2,50 +2,42 @@
 
 import json
 
-import tornado.ioloop
-import tornado.web
-import tornado.httpserver
 
 from FetchStats.Plugins import *
+
 import FetchStats
 
+# TODO: logging!
 
-class StatsHandler(tornado.web.RequestHandler):
+
+class StatsApp(object):
     """
     Gets and returns the stats.
     """
 
+    # load modules to run
     _plugins = FetchStats.Fetcher.get_plugins()
 
-    def get(self):
+    def __call__(self, environ, start_response):
         result = {}
-        # 1. load modules to run
-        #     - handled in the '... Plugins import *' line
 
-        # 2. Execute each (as a coroutine?)
-        #     - currently handled during instantiation
-
+        # Execute each plugin
         for plugin in self._plugins:
             context = plugin.context
             result[context] = plugin.dump()
-        # 3. Return the json all together
-        self.set_header("Content-Type", "application/json")
-        self.write(json.dumps(result))
 
-
-application = tornado.web.Application([
-    (r'/', StatsHandler),
-])
+        # Return the json all together
+        start_response("200 OK", [("Content-Type", "application/json")])
+        return json.dumps(result)
 
 
 if __name__ == "__main__":
     print "plugins loaded..."
     print "server listening on http://0.0.0.0:8888"
     try:
-        server = tornado.httpserver.HTTPServer(application)
-        server.bind(8888)
-        server.start(0)
-        tornado.ioloop.IOLoop.instance().start()
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 8888, StatsApp())
+        httpd.serve_forever()
     except KeyboardInterrupt:
         print "shutting down..."
-        exit(0)
+        raise SystemExit(0)
