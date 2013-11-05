f13e9adf2a1e2b51554afafdde13ee344d31d2ed
Tim Bielawa <tbielawa@redhat.com>
2013-11-05 10:36:20 -0500

    Add a DEB fact provider for listing packages on debian systems

---
+++ FetchStats/Plugins/DEB.py
@@ -0,0 +1,38 @@
+import datetime
+from FetchStats import Fetcher
+
+
+class DEB(Fetcher):
+
+    def __init__(self):
+        """
+        Returns a deb  manifest (all debs installed on the system)
+
+        **Note**: This takes more than a few seconds!!
+        """
+        self.context = 'deb'
+        self._load_data()
+
+    def _load_data(self):
+        self._refresh_time = datetime.datetime.utcnow()
+        self._debs = {}
+
+        cmd = 'dpkg-query -W'
+
+        try:
+            for line in self._exec(cmd).split('\n')[:-1]:
+                (deb_name, deb_version) = line.split()
+                self._debs[deb_name] = deb_version
+            self._loaded(True)
+        except Exception, e:
+            self._loaded(False, str(e))
+
+    def dump(self, key=None):
+        # poor mans cache, refresh cache in an hour
+        if (datetime.datetime.utcnow() -
+                datetime.timedelta(hours=1)) > self._refresh_time:
+            self._load_data(key)
+        return self._debs
+
+    def dump_json(self, key=None):
+        return self.json.dumps(self.dump(key))
