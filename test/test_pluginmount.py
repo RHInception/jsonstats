40ae95e87e439645d35376942f8c48ce9e62b2ad
Tim Bielawa <tbielawa@redhat.com>
2013-11-19 15:47:46 -0500

    Fix the plugin mount text. And make it way more intelligent.

--- test/test_pluginmount.py
+++ test/test_pluginmount.py
@@ -1,5 +1,3 @@
-from JsonStats.FetchStats.Plugins import *
-
 from . import TestCase
 
 import JsonStats.FetchStats.Plugins
@@ -12,11 +10,25 @@ class TestPluginMount(TestCase):
         # Do stuff that has to happen on every test in this instance
         self.fetcher = Fetcher
 
+        class _example_plugin(Fetcher):
+            def __init__(self):
+                self.context = 'testplugin'
+                self._load_data()
+            def _load_data(self):
+                self._loaded(True)
+            def dump(self):
+                return {}
+            def dump_json(self):
+                return self.json.dumps(self.dump())
+
+        self.example_plugin = _example_plugin
+
+
     def test_get_plugins(self):
         """
         Verify that after loading plugins we can see them attached to
         the Mount.
         """
+        example_plugin = self.example_plugin()
         discovered = len(self.fetcher.get_plugins())
-        expected = len(JsonStats.FetchStats.Plugins.__all__)
-        self.assertEqual(discovered, expected)
+        assert discovered == 1
