e469ae00a815b25ab7d0e45c7f8076c56ae834b4
Tim Bielawa <tbielawa@redhat.com>
2014-02-17 08:48:28 -0500

    Implement unittest.TestCase.assertGreaterEqual for pythons less than 2.7

--- test/__init__.py
+++ test/__init__.py
@@ -7,3 +7,13 @@ class TestCase(unittest.TestCase):
     Parent TestCase to use for all tests.
     """
     pass
+
+    def assertGreaterEqual(self, first, second, msg=None):
+        """
+        Test that first is respectively >= than second
+        depending on the method name. If not, the test will fail.
+        """
+        if first >= second:
+            pass
+        else:
+            self.fail(msg=msg)
